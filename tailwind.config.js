d723f35da892765c1e9fd0de1b6ce2839656d618
Zak Farnworth <zak@farnworth.org.uk>
2024-12-20 23:35:27 +0000

    Fix 5

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+    './lib/**/*.{ts,tsx}',
+  ],
   theme: {
     container: {
       center: true,
