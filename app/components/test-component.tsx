66c73a1adccb373d16d395f963cd3d06bdd8fdf7
Zak Farnworth <130570087+zakfarnworth23@users.noreply.github.com>
2025-03-30 18:08:44 +0100

    Delete app directory

--- app/components/test-component.tsx
+++
@@ -1,8 +0,0 @@
-export function TestComponent() {
-  return (
-    <div className="p-4 m-4 bg-blue-500 text-white rounded-lg">
-      This site is still under development.
-    </div>
-  )
-}
-
--- app/lib/definitions.ts
+++
@@ -1,9 +0,0 @@
-export type Blog = {
-  id: string
-  title: string
-  content: string
-  createdAt: Date
-  updatedAt: Date
-  published: boolean
-}
-
