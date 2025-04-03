3f0a7b1d9279600231f8f7ac910f011aa237a44d
Zak Farnworth <130570087+zakfarnworth23@users.noreply.github.com>
2025-04-03 13:41:46 +0100

    Update about-section.tsx

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function AboutSection() {
   return (
     <section id="about" className="py-8">
-      <h2 className="text-2xl font-bold mb-6 border-b-2 border-gray-200 pb-2">About Zak Farnworth</h2>
+      <h2 className="text-2xl font-bold mb-6 border-b-2 border-gray-200 pb-2">Welcome</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         <div className="md:col-span-1">
           <Image
